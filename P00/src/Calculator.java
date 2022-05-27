db77fafa0a492f8974a673e44d044a608269d458
21009490 <21009490@LAPTOP-CIOMEQ9T>
2022-05-27 14:49:17 +0800

    first push to P00 respository in GitHub

--- P00/src/Calculator.java
+++ P00/src/Calculator.java
@@ -1,27 +1,4 @@
 
 public class Calculator {
-	public int add(int a, int b) {
-		return a + b;
-	}
-	
-	public int subtract(int a, int b) {
-		return a - b;
-	}
-	
-	public int multiple(int a, int b) {
-		return a * b;
-	}
-	
-	public int divide(int a, int b) {
-		return a / b;
-	}
-	
-	public boolean testDivideWith0Denominator(int a, int b) {
-		if (b == 0) {
-			return false;
-		} else {
-			return true;
-		}
-		
-	}
+
 }
--- P00/test/CalculatorTest.java
+++ P00/test/CalculatorTest.java
@@ -1,82 +1,22 @@
 import static org.junit.Assert.*;
 
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class CalculatorTest {
 
-	int a, b;
-	Calculator cal = new Calculator();
-
 	@Before
 	public void setUp() throws Exception {
-		a = 4321;
-		b = 1234;
-	}
-
-	// Test for Addition method
-	@Test
-	public void testAdd() {
-		// fail("Not yet implemented");
-
-		int actual = cal.add(a, b);
-
-		int expected = 5555;
-		assertEquals(expected, actual);
-	}
-
-	// Test for subtraction method
-	@Test
-	public void testSubtract() {
-
-		int actual = cal.subtract(a, b);
-
-		int expected = 3087;
-		assertEquals(actual, expected);
-	}
-
-	// Test for multiplication method
-	@Test
-	public void testMultiple() {
-
-		int actual = cal.multiple(a, b);
-
-		int expected = 5332114;
-		assertEquals(actual, expected);
-	}
-
-	// Test for division method
-	@Test
-	public void testDivide() {
-
-		int actual = cal.divide(a, b);
-
-		int expected = 3;
-		assertEquals(actual, expected);
-	}
-	
-	// Test if denominator is 0
-	public void testDivideWith0Denominator() {
-		
-		try {
-			Calculator cal = new Calculator();
-			cal.divide(a, b);
-			fail("Expected an IllegalArgumentException to be thrown");
-		} catch (IllegalArgumentException e) {
-			assertEquals("Division by zero is not supported", e.getMessage());
-		} catch(Throwable t) {
-			assertEquals("Expected an IllegalArgumentException to be thrown", t.getMessage());
-		}
-		
 	}
 
 	@After
 	public void tearDown() throws Exception {
 	}
 
-
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
 
 }
